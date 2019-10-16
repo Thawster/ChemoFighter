d42ffdf30ddf1a4e544e8eb9f41aa089f9271797
Thawkster <tristan@nithon.com>
2019-10-16 11:39:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ from pymongo import MongoClient
 from bson.objectid import objectId
 import os
 
+host = os.environment.get('MONGODB_URI', 'mongodb://localhost:27017/ChemoFighter')
+client = MongoClient(host=host)
+db = client.get_default_database()
+roster = db.roster
+
 app = Flask(__name__)
 
 @app.route('/')
 def main_menu():
     """Home Screen"""
-    return render_template('chemo_index.html')
+    return render_template('chemo_index.html' roster=roster)
+
+@app.route('/roster', methods=['POST'])
+def roster_submit():
+    """submit a new Character"""
+    
