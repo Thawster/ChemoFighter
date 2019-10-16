b77ec29199f82efda6d16abea3c276f3a6023d5b
Thawkster <tristan@nithon.com>
2019-10-16 12:14:57 -0700

    [ADD] submitting characters

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import objectId
 import os
 from character import Character, Ability, Armor
+from arena import Team, Arena
 
 host = os.environment.get('MONGODB_URI', 'mongodb://localhost:27017/ChemoFighter')
 client = MongoClient(host=host)
@@ -19,3 +20,22 @@ def main_menu():
 @app.route('/roster', methods=['POST'])
 def roster_submit():
     """submit a new Character"""
+    character = {
+        'name': request.form.get('name'),
+        'starting health': request.form.get('starting health')
+    }
+    roster.insert_one(character)
+    return redirect(url_for('chemo_index'))
+
+@app.route('/roster/<character_id>', methods=['POST'])
+def roster_update():
+    """submit a edited Character"""
+    updated_character = {
+        'name': request.form.get('name'),
+        'starting health': request.form.get('starting health')
+    }
+    roster.update_one(
+        {'_id': ObjectId(character_id)},
+        {'$set': updated_item})
+    return redirect(url_for('roster_show', item_id=item_id))
+    )
